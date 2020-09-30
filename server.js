07eaae25795f2428d2f3ef42da3f2b6619bfb8e1
wic0144 <Arm_ruttanard@hotmail.com>
2020-10-01 01:43:49 +0700

    tset

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
-
+const path = require('path');
+const PORT = process.env.PORT || 5000;
 app.get('/', (req, res) => {
   res.send('Hello World')
 })
